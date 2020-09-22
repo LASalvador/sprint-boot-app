d403c04eb49e06ff92ccb8c9850a003a0421ab92
LASalvador <lucas.salvador11358@gmail.com>
2020-09-22 13:46:39 +0000

    adicionando relacionamento de usuario e autorizacao

--- src/main/java/br/gov/sp/fatec/sprintapp/entity/Autorizacao.java
+++ src/main/java/br/gov/sp/fatec/sprintapp/entity/Autorizacao.java
@@ -4,9 +4,11 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,15 +20,15 @@ public class Autorizacao {
 	private Long id;
 	@Column(name="aut_nome")
 	private String nome;
+	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "autorizacoes")
+    private Set<Usuario> usuarios;
 	
-//	private Set<Usuario> usuarios;
-	
-//	public Set<Usuario> getUsuarios() {
-//		return usuarios;
-//	}
-//	public void setUsuarios(Set<Usuario> usuarios) {
-//		this.usuarios = usuarios;
-//	}
+    public Set<Usuario> getUsuarios() {
+    	return usuarios;
+    }
+    public void setUsuarios(Set<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
 	public Long getId() {
 		return id;
 	}
