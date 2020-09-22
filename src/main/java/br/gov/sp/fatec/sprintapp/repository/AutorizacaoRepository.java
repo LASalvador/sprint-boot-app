f9d1638b7aed5892926c7f6bc8077b10471aa835
LASalvador <lucas.salvador11358@gmail.com>
2020-09-22 13:47:02 +0000

    criando autorizacao repository

---
+++ src/main/java/br/gov/sp/fatec/sprintapp/repository/AutorizacaoRepository.java
@@ -0,0 +1,9 @@
+package br.gov.sp.fatec.sprintapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.gov.sp.fatec.sprintapp.entity.Autorizacao;
+
+public interface AutorizacaoRepository extends JpaRepository<Autorizacao, Long> {
+
+}
